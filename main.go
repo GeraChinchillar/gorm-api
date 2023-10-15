849991fe9856632a0a7b74d9f97be688e7d1c867
Gerardo Chinchilla <00049518@uca.edu.sv>
2023-10-14 23:51:52 -0600

    adding gorm info and routes

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gerachinchillar/gorm-api/db"
+	"github.com/gerachinchillar/gorm-api/model"
 	"github.com/gerachinchillar/gorm-api/routes"
 	"github.com/gorilla/mux"
 )
@@ -12,9 +13,17 @@ func main() {
 
 	db.DBConnection()
 
+	db.DB.AutoMigrate(model.Task{})
+	db.DB.AutoMigrate(model.User{})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
+
+	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	r.HandleFunc("/users", routes.DeleteUsersHandler).Methods("DELETE")
 
 	http.ListenAndServe(":3000", r)
 }
