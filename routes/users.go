269ddeef990d0b0799f5ea5ced62f070584c8a79
Gerardo Chinchilla <00049518@uca.edu.sv>
2023-10-15 22:40:41 -0600

    adding integration getUsers and postUser

--- routes/users.go
+++ routes/users.go
@@ -1,9 +1,17 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gerachinchillar/gorm-api/db"
+	"github.com/gerachinchillar/gorm-api/model"
+)
 
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("get users"))
+	var users []model.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
 }
 
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +19,18 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("post"))
+	var user model.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	createdUser := db.DB.Create(&user)
+	err := createdUser.Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	json.NewEncoder(w).Encode(&user)
 }
 
 func DeleteUsersHandler(w http.ResponseWriter, r *http.Request) {
